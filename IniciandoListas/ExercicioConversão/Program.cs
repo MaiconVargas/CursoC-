d21cac569f4524e51920862a016bc7b1336cc178
Maicon Cristian <maiconcristian17@outlook.com.br>
2019-09-17 13:59:36 -0300

    Aula17/09

---
+++ IniciandoListas/ExercicioConversão/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioConversão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversao();
+        }
+
+        private static void Conversao()
+        {
+            var Valor = new List<double>();
+
+            Valor.Add(5);
+            Valor.Add(8);
+            Valor.Add(10);
+            Valor.Add(0.05);
+
+            Valor.ForEach(ValoresCon => Console.WriteLine(ValoresCon.ToString("C2")+" "+ FormataNumeroDecimalEmEuro(ValoresCon)+ " " + FormataNumeroDecimalEmBitCoin(ValoresCon)+ " "+ FormataNumeroDecimalEmYen(ValoresCon)));
+
+            Console.ReadKey();
+        }
+
+        private static string FormataNumeroDecimalEmEuro(double meuNumero)
+        {
+            return (meuNumero / 4.53).ToString("C", CultureInfo.CreateSpecificCulture("en-US"))
+                .Replace("$","Euro");
+        }
+       
+        private static string FormataNumeroDecimalEmBitCoin(double meuNumero)
+        {
+            return  (meuNumero / 41733.86).ToString("C", CultureInfo.CreateSpecificCulture("en-US")).Replace("$","BTC");
+        }
+        private static string FormataNumeroDecimalEmYen(double meuNumero)
+        {
+            return (meuNumero / 0.038).ToString("C5", CultureInfo.CreateSpecificCulture("ja-JP"));
+        }
+    }
+}
---
+++ IniciandoListas/ForeachNaLista/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachNaLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaDeDecimais();
+
+        }
+
+        
+        
+        
+        /// <summary>
+        /// Mostra listas definidas. Data e String.
+        /// </summary>
+
+
+        private static void ListaDataTime()
+        {
+            var minhaLista = new List<DateTime>();
+
+            minhaLista.Add(new DateTime(2019, 9, 17));
+            minhaLista.Add(new DateTime(2019, 9, 18));
+            minhaLista.Add(new DateTime(2019, 9, 19));
+            minhaLista.Add(new DateTime(2019, 9, 20));
+
+            minhaLista.ForEach(minhaData => Console.WriteLine(minhaData.ToString("yyyy/MMM/dd")));
+
+            Console.ReadKey();
+        }
+
+        private static void ListaString ()
+        {
+            var minhaLista = new List<string>();
+
+            minhaLista.Add("XIAOMI");
+            minhaLista.Add("IPHONE");
+            minhaLista.Add("SANSUMG");
+            minhaLista.Add("NOKIA");
+
+            minhaLista.ForEach(i => Console.WriteLine(i));
+
+            Console.ReadKey();
+
+        }
+
+        private static void ListaDeInteiros ()
+
+        {
+            var minhaLista = new List<int>();
+
+            minhaLista.Add(17);
+            minhaLista.Add(18);
+            minhaLista.Add(19);
+            minhaLista.Add(20);
+
+            minhaLista.ForEach(meuNumero => Console.WriteLine(meuNumero.ToString("C2")));
+
+            Console.ReadKey();
+        }
+
+        private static void ListaDeDecimais ()
+
+        {
+            var minhaLista = new List<double>();
+
+            minhaLista.Add(3.75);
+            minhaLista.Add(4.75);
+            minhaLista.Add(2.63);
+            minhaLista.Add(0.01);
+
+            minhaLista.ForEach(meuDecimal => Console.WriteLine(meuDecimal.ToString("C2")));
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
